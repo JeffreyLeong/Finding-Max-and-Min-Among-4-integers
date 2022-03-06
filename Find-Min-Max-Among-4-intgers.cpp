e5e40a4f9a762bf76f479377e8571ce64e8327bc
dollarortwo <jleong001@gmail.com>
2022-03-05 16:33:26 -0800

    added comments and do-while loop

--- Find-Min-Max-Among-4-intgers.cpp
+++ Find-Min-Max-Among-4-intgers.cpp
@@ -6,9 +6,14 @@ void order_num(int& max, int& min);
 
 int main()
 {
-    int a=20, b=120;
-    order_num(a, b);
-    cout << a << ", " << b << endl;
+    char repeat;
+    // Loop the program until the users says yes or no
+    do{
+
+    } while ((repeat='Y') || (repeat = 'y'));
+        // int a=20, b=-9;
+        // order_num(a, b);
+        // cout << a << ", " << b << endl;
  return 0;
 }
 
